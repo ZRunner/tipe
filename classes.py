a9136edb4807b56a029ce27388c9e64de8e69b1b
ZRunner <z.runner.mc@gmail.com>
2019-10-04 23:11:10 +0200

    suppression des vecteurs + ajout du raytracing

--- classes.py
+++ classes.py
@@ -1,36 +1,45 @@
 import pygame
+import math
+
+vector = pygame.math.Vector2
 
 
-class Vector:
-    """Un vecteur mathematique
-    Deux manieres de le creer:
-    - avec sa norme en x et en y (float/int)
-    - avec deux coordonnees de points A et B (tuple)"""
+def lineRayIntersectionPoint(rayOrigin: tuple, rayDirection: tuple, point1: tuple, point2: tuple):
+    """Donne le vecteur allant de l'origine vers le point d'intersection
+    Source: http://bit.ly/2VfSFOV
+    Tous les arguments doivent être de la forme (x,y)"""
+    # Convert to vectors
+    rayOrigin = vector(rayOrigin)
+    rayDirection = vector(rayDirection).normalize()
+    point1 = vector(point1)
+    point2 = vector(point2)
 
-    def __init__(self, **kwargs):
-        if "x" in kwargs.keys() and "y" in kwargs.keys():
-            assert isinstance(kwargs['x'], (int, float)) and isinstance(
-                kwargs['y'], (int, float)), "x et y doivent être des nombres valides"
-            self.x = kwargs['x']
-            self.y = kwargs['y']
-        elif "A" in kwargs.keys() and "B" in kwargs.keys():
-            assert isinstance(kwargs['A'], (tuple, list)) and isinstance(
-                kwargs['B'], (tuple, list)) and len(kwargs['A']) == len(kwargs['B']) == 2, "A et B doivent être des tuples de longueur 2"
-            self.x = abs(kwargs['A'][0] - kwargs['B'][0])/2
-            self.y = abs(kwargs['A'][1] - kwargs['B'][1])/2
-        else:
-            raise TypeError("Missing arguments")
+    # Ray-Line Segment Intersection Test in 2D
+    # http://bit.ly/1CoxdrG
+    v1 = rayOrigin - point1
+    v2 = point2 - point1
+    v3 = vector([-rayDirection[1], rayDirection[0]])
+    try:
+        t1 = v2.cross(v1) / v2.dot(v3)
+        t2 = v1.dot(v3) / v2.dot(v3)
+    except ZeroDivisionError:
+        return []
+    if t1 >= 0.0 and t2 >= 0.0 and t2 <= 1.0:
+        return [rayOrigin + t1 * rayDirection]
+    return []
 
 
 class Car:
     """Represente une voiture
     color represente la couleur de la voiture, de type pygame.Color"""
 
-    def __init__(self, color: pygame.Color = pygame.Color(255, 0, 0)):
+    def __init__(self, color: pygame.Color = pygame.Color(255, 0, 0), abs_rotation:float = -90):
         """Initialise la voiture
-        - color (pygame.Color): couleur de la voiture [par défaut rouge]"""
+        - color (pygame.Color): couleur de la voiture [par défaut rouge]
+        - abs_rotation (float): rotation par rapport au plan de la voiture [par défaut sud]"""
         self.color = color
         self.position = [0, 0]
+        self.abs_rotation = abs_rotation
 
     def set_position(self, x: int, y: int):
         """Modifie la position absolue de la voiture
@@ -38,11 +47,37 @@ class Car:
         - y (int): nouvelle position de la voiture sur l'axe Y (ordonnée)"""
         self.position = [x, y]
 
-    def apply_vector(self, vector: Vector):
+    def apply_vector(self, vector: pygame.math.Vector2):
         """Applique un vecteur à la position de la voiture
         - vector (Vector): vecteur à appliquer"""
         self.position[0] += vector.x
         self.position[1] += vector.y
+
+    def raytrace(self, angle: int, circuit: list, max_distance: int = 200, use_absolute_angle: bool = False, return_real_distance: bool = False):
+        """Vérifie si le rayon d'angle donné rencontre un mur avant une certaine distance
+        - angle (int): angle du rayon en degrés, 0 étant l'avant de la voiture
+        - circuit (list): liste de tous les murs à prendre en compte, de type Border
+        - max_distance (int): distance maximum à prendre en compte [par défaut 200]
+        - use_absolute_angle (bool): si l'angle donné est relatif au plan (1) ou à la voiture (0) [par défaut False]
+        - return_real_distance (bool): si la valeur retournée doit être la distance réelle, et non entre 0 et 1 [par défaut False]
+
+        Retourne un float entre 0 et 1, 0 étant une collision immédiate et 1 à la distance maximum, ou -1 si aucune collision"""
+        assert all([isinstance(x, Border) for x in circuit]), "La liste du circuit ne doit contenir que des objets de type Border"
+        if not use_absolute_angle:
+            angle = self.abs_rotation + angle
+        angle = math.radians(angle)
+        direction = vector(round(math.cos(angle),5), round(math.sin(angle),5))
+        distances = [lineRayIntersectionPoint(self.position, direction, (
+            line.start[0], line.start[1]), (line.end[0], line.end[1])) for line in circuit]
+        distances = [x[0].length() for x in distances if len(x)>0]
+        if len(distances) == 0:
+            return -1
+        shortest_distance = min(distances)
+        if shortest_distance > max_distance:
+            return -1
+        if return_real_distance:
+            return shortest_distance
+        return shortest_distance/max_distance
 
 
 class Border:
@@ -55,3 +90,10 @@ class Border:
         self.color = color
         self.start = A
         self.end = B
+
+
+# # -- Tests de raytracing -- #
+# borders = [Border((8,10),(-2,10)), Border((5,9),(-6,7))]
+# car = Car(abs_rotation=60)
+# car.set_position(1,0.5)
+# print(car.raytrace(0, borders, max_distance=12, return_real_distance=True))
