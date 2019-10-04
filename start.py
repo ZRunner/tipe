30420658a903935a3478f4ffaf8b1b976266c65a
ZRunner <z.runner.mc@gmail.com>
2019-10-04 18:14:58 +0200

    premier commit

---
+++ start.py
@@ -0,0 +1,12 @@
+
+def main():
+    print("""Lancement du programme
+
+    Assurez-vous que toutes les dépendances utilisées soient installées sur votre ordinateur ;
+    si besoin entrez `pip install -r requirements.txt` dans votre console
+    """)
+
+
+
+if __name__=="__main__":
+    main()
