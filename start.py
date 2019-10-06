30c2612af25fcb4845d73f7352b3be46d9c77e07
Z_runner <arthur.blaise.23@gmail.com>
2019-10-06 15:53:29 +0200

    ajout des voitures et du contrôle manuel

--- start.py
+++ start.py
@@ -1,7 +1,9 @@
 import draw
 import key
 from classes import *
+import settings
 import pygame
+
 
 def main():
     print("""Lancement du programme
@@ -11,21 +13,38 @@ def main():
     """)
 
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((800,800))
     pygame.display.set_caption("TIPE")
     circuit = [Border((10,10),(10,100)),Border((10,100),(70,200)),Border((70,200),(170,200))]
-
-     
+    if settings.manual_control:
+        cars = [Car()]
+    else:
+        cars = [Car() for _ in range(settings.cars_number)]
 
     running = True
     screen.fill((255,255,255))
+    dt = 1
     while running:
-    	for event in pygame.event.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
-            	running = False 
-    	draw.circuit(screen,circuit)
-    	pygame.display.flip()
-
+                running = False
+        draw.circuit(screen,circuit)
+        pygame.display.flip()
+        # Gestion du mouvement de la voiture
+        if settings.manual_control:
+            pressed = pygame.key.get_pressed()
+            move_vector = pygame.math.Vector2()
+            for m in (settings.move_keys[key] for key in settings.move_keys if pressed[key]):
+                move_vector += m
+            # Normalisation du vecteur si nécessaire
+            if move_vector.length() > 0:
+                move_vector.normalize_ip()
+                cars[0].apply_vector(move_vector*dt*settings.fps/1000)
+                # print(cars[0].position)
+        dt = clock.tick(settings.fps)
+    pygame.quit()
+        
 
 if __name__=="__main__":
     main()
